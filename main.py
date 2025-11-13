382631b27087268ef0d35058ba466ecd7455aead
crisgallego126-rgb <crisgallego126@gmail.com>
2025-11-13 00:37:34 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,91 @@
+import streamlit
+import groq
+
+#VARIABLES
+altura_contenedor_chat = 600
+stream_status = True
+
+#CONSTANTES
+MODELOS = ["llama-3.1-8b-instant", "llama-3.3-70b-versatile", "llama-guard-4-12b"]
+
+#FUNCIONES
+
+#ESTA FUNCION NO UTILIZA STREAMLIT PAEA CREAR LA INTERFAZ DE LA PAGINA T ADEMAS RETORNA EL
+#MODELO ELEGIDO POR EL USUARIO
+def configurar_pagina():
+    
+    streamlit.set_page_config(page_title="El chat piola de Cristian")
+    
+    streamlit.title("El chat piola de Cristian")
+    
+    streamlit.sidebar.title("Seleccion de modelo")
+    
+    elegir_modelo = streamlit.sidebar.selectbox("Elegi un modelo", options=MODELOS, index=0)
+    
+    return elegir_modelo
+
+#ESTA FUNCION LLAMA A st.secrets PARA OBTENER LA CLAVE DE LA API DE GROQ Y CREA UN USUARIO
+def crear_usuario():
+    clave_secreta = streamlit.secrets["CLAVE"]
+    return groq.Groq(api_key = clave_secreta)
+
+#CONFIGURA EL MODELO DE LEMGUAJE PARA QUE PROCESE EL PROMPT DEL USUARIO
+def configurar_modelo(cliente, modelo_elegido, prompt_usuario):
+    return cliente.chat.completions.create(
+        model = modelo_elegido,
+        messages = [{"role" : "user" , "content" : prompt_usuario}],
+        stream = stream_status
+    )
+    
+#CREAMOS UNA SESION LLAMADA "mensajes" QUE VA A GUARDAR LO QUE LE ESCRIBIMOS AL CHATBOT
+def inicializar_estado():
+    if "mensajes" not in streamlit.session_state:
+        streamlit.session_state.mensajes = []
+        
+def actualizar_historial(rol, contenido, avatar):
+    streamlit.session_state.mensajes.append({"role" : rol, "content" : contenido, "avatar" : avatar})
+    
+def mostrar_historial():
+    for mensaje in streamlit.session_state.mensajes:
+        with streamlit.chat_message(mensaje ["role"], avatar=mensaje ["avatar"]):
+            streamlit.write(mensaje ["content"])
+
+def area_chat():
+    contenedor = streamlit.container (height=altura_contenedor_chat, border=True)
+    with contenedor:  
+        mostrar_historial()
+
+def generar_respuesta (respuesta_completa_del_bot):
+    _respuesta_posta = ""
+    for frase in respuesta_completa_del_bot:
+        if frase.choices [0].delta.content:
+            _respuesta_posta += frase.choices[0].delta.content
+            yield frase.choices[0].delta.content
+    return _respuesta_posta
+
+#---------------------------IMPLEMENTACION-------------------------------------
+
+def main():
+    modelo_elegido_por_el_usuario = configurar_pagina()
+
+    cliente_usuario = crear_usuario()
+        
+    inicializar_estado()
+
+    area_chat()
+
+    promt_del_usuario = streamlit.chat_input("Escriba tu prompt: ")
+
+    if promt_del_usuario:
+        actualizar_historial("usuario", promt_del_usuario, "🙂")
+        respuesta_del_bot = configurar_modelo(cliente_usuario, modelo_elegido_por_el_usuario, promt_del_usuario)
+
+        if respuesta_del_bot:
+            with streamlit.chat_message("assistant"):
+                respuesta_posta = streamlit.write_stream(generar_respuesta (respuesta_del_bot))
+                actualizar_historial("assistant", respuesta_posta, "🙂")
+                
+                streamlit.rerun()
+                
+if __name__== "__main__":
+     main()
